8c19e7180322f9541fce3b62ac94348e850a51de
zaid <zaidk22@gmail.com>
2023-09-21 17:29:14 +0530

    video cache

--- lib/domain/core/constants/api_constants.dart
+++ lib/domain/core/constants/api_constants.dart
@@ -5,5 +5,5 @@ static String baseUrl = '';
   static const String signUp = "";
    static const String signIn = "";
       static const String forgotPassword = "";
-      static const String videoUrl = "http://staging.indemandtickets.co.uk//frontend/web/backend/images/landing_video.webm";
+      static const String videoUrl = "http://staging.indemandtickets.co.uk//frontend/web/backend/images/landing_video.mp4";
 }
